cc2d12e6214ce8de7580f9d58b96a425b796a5db
Manav Oza <manavoza99@gmail.com>
2019-10-23 16:01:46 +0530

    FIrst Challenge Accepted

---
+++ code #1.js
@@ -0,0 +1,12 @@
+// Defintion: Make a program which returns 1 is you pass value 2 and returns 2 if you pass value 1
+
+// Code to Break the Load
+// Credit Goes to -> Navit Bro :-)
+
+const probability = [0,2,1];
+
+function fun (i) {
+    return probability[i];
+}
+
+console.log(fun(2));
